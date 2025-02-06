9b816fd8a67c7893a57216437eabaf066c48de7a
Kelton Zinn <kzinn2@academic.rrc.ca>
2025-02-05 23:21:00 -0600

    Added variables and output

--- zinn_k_assignment_2.py
+++ zinn_k_assignment_2.py
@@ -1,6 +1,43 @@
-""" Assignment_02 source code
+""" Assignment_02 source code"""
 
 __author__ = "Kelton Zinn"
 
 __version__ = "1.0.0"
-"""
+
+# SIMPLE DATA TYPES
+
+name = "Kelton"
+
+print(type(name))
+
+print("name:", name, "type:", type(name))
+
+has_license = True
+
+print("has license:", has_license, "type:", type(has_license))
+
+Year = 2025
+
+print("This year:", Year, "type:", type(Year))
+
+Next_year = Year + 1
+
+print("Next year:", Next_year, "type:", type(Next_year))
+
+# MATHEMATICAL OPERATIONS
+
+GST = 0.05
+
+PST = 0.07
+
+vehicle_price = 80000
+
+federal_tax = vehicle_price * GST
+
+provincial_tax = vehicle_price * PST
+
+final_cost = federal_tax + provincial_tax + vehicle_price
+
+print("Purchase price:", vehicle_price, "Provincial tax:", provincial_tax, "Federal tax:", federal_tax, "Total:", final_cost)
+
+print(f"Purchase price: ${vehicle_price:,.2f}", f"Provincial tax: ${provincial_tax:,.2f}", f"Federal tax: ${federal_tax:,.2f}", f"Total: ${final_cost:,.2f}")
